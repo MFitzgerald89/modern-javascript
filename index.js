d8a5eb834d5f212e0bea6a4d3e92fc696f8a7bd0
MFitzgerald89 <122069535+MFitzgerald89@users.noreply.github.com>
2023-04-06 16:09:12 -0400

    adds webpack and json package

--- index.js
+++ index.js
@@ -1,33 +1,14 @@
 var moment = require("moment");
 
-console.log("Hey there!");
+console.log("Hey bro!");
 console.log(moment().endOf("day").fromNow());
 
-var name = "Mike";
+var name = "Crispin";
 var time = "this loveliest of days";
 console.log(`Hello ${name}, how are you ${time}?`);
 
 var question = "Who is your daddy and what does does he do? ";
 
-var answer = "I don't have a daddy...";
+var answer = "He is a man who loves dancing...";
 
 console.log(`${question}` + `${answer}`);
-
-var fibonacci = {
-  [Symbol.iterator]: function* () {
-    var pre = 0,
-      cur = 1;
-    for (;;) {
-      var temp = pre;
-      pre = cur;
-      cur += temp;
-      yield cur;
-    }
-  },
-};
-
-for (var n of fibonacci) {
-  // truncate the sequence at 1000
-  if (n > 1000) break;
-  console.log(n);
-}
